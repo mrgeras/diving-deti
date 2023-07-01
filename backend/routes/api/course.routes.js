8a812909a8c7b2a68a6c505e071e94cbed972e5c
mrgeras <gerasimbek3@gmail.com>
2023-07-01 17:51:37 +0300

    1

--- backend/routes/api/course.routes.js
+++ backend/routes/api/course.routes.js
@@ -3,11 +3,10 @@ const { Course } = require('../../db/models');
 
 router.get('/', async (req, res) => {
   try {
-    const course = await Course.findAll({
-    });
-    console.log(course)
+    const course = await Course.findAll({});
+    console.log(course);
 
-    res.json(course );
+    res.json(course);
   } catch (err) {
     console.log(err);
     res.json({ message: err.message });
