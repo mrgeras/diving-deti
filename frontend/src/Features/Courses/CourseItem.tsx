aaa99a45ac4c247946e697001c1a1c07e681d126
mrgeras <gerasimbek3@gmail.com>
2023-07-02 11:24:39 +0300

    about button and function

--- frontend/src/Features/Courses/CourseItem.tsx
+++ frontend/src/Features/Courses/CourseItem.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Course } from './types/CoursesType';
 import './CourseItem.css';
 
+function getAbout({
+  course,
+  navigate,
+}: {
+  course: Course;
+  navigate: (value: string) => void;
+}): void {
+  navigate(`/courses/${course.id}`);
+}
+
 function CourseItem({ course }: { course: Course }): JSX.Element {
+  const navigate = useNavigate();
+
   return (
     <div className="course-card-container">
       <img className="course-card" src={course.course_img} alt="" />
       <p>{course.course_name}</p>
       <p>{course.description}</p>
-      <button type="button">Подробнее</button>
+      <button
+        className="about-btn"
+        onClick={() => getAbout({ course, navigate })}
+        type="button"
+      >
+        Подробнее
+      </button>
     </div>
   );
 }
