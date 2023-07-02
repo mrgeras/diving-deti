5245714c7a221f2fed69430278138b6b34f46f1d
mrgeras <gerasimbek3@gmail.com>
2023-07-02 19:36:49 +0300

    articles

--- frontend/src/Features/Courses/CoursesList.tsx
+++ frontend/src/Features/Courses/CoursesList.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../Redux/store';
 import CourseItem from './CourseItem';
 import { coursesInit } from '../../Redux/Reducers/CourseSlice';
-// import './CourseList.css';
+import './CourseList.css';
 
 function CoursesList(): JSX.Element {
   const { courses } = useAppSelector((store) => store.courses);
