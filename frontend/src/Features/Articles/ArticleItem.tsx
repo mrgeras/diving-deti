9730fc7f6e755f3546f39554a7c355d74f355894
mrgeras <gerasimbek3@gmail.com>
2023-07-03 12:52:24 +0300

    fix about

--- frontend/src/Features/Articles/ArticleItem.tsx
+++ frontend/src/Features/Articles/ArticleItem.tsx
@@ -1,33 +1,17 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Article } from './types/ArticlesType';
 import './ArticleItem.css';
 
-function getAbout({
-  article,
-  navigate,
-}: {
-  article: Article;
-  navigate: (value: string) => void;
-}): void {
-  navigate(`/article/${article.id}`);
-}
-
 function ArticleItem({ article }: { article: Article }): JSX.Element {
-  const navigate = useNavigate();
-
   return (
     <div className="article-card">
       <a className="about-article-card">
         <img className="article-card-img" src={article.articleImg} alt="" />
         <p>{article.articleName}</p>
         <p>{article.articleText}</p>
-        <button
-          className="about-article-btn"
-          onClick={() => getAbout({ article, navigate })}
-          type="button"
-        >
-          Подробнее
+        <button className="about-article-btn" type="button">
+          <Link to={`/articles/${article.id}`}>Подробнее</Link>
         </button>
       </a>
     </div>
--- frontend/src/Features/Courses/CourseItem.tsx
+++ frontend/src/Features/Courses/CourseItem.tsx
@@ -1,32 +1,17 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Course } from './types/CoursesType';
 import './CourseItem.css';
 
-function getAbout({
-  course,
-  navigate,
-}: {
-  course: Course;
-  navigate: (value: string) => void;
-}): void {
-  navigate(`/courses/${course.id}`);
-}
-
 function CourseItem({ course }: { course: Course }): JSX.Element {
-  const navigate = useNavigate();
-
   return (
     <div className="course-card">
       <img className="course-card-img" src={course.courseImg} alt="" />
       <p>{course.courseName}</p>
       <p>{course.description}</p>
-      <button
-        className="about-btn"
-        onClick={() => getAbout({ course, navigate })}
-        type="button"
-      >
-        Подробнее
+      <button className="about-btn" type="button">
+        {' '}
+        <Link to={`/courses/${course.id}`}>Подробнее</Link>
       </button>
     </div>
   );
