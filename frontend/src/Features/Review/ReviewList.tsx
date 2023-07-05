dac80a91da038e0a5c23cc3b3547801b58a5403d
pu99r <iiigor.mik@gmail.com>
2023-07-05 12:16:07 +0300

    otzov

--- frontend/src/Features/Review/ReviewList.tsx
+++ frontend/src/Features/Review/ReviewList.tsx
@@ -1,20 +1,56 @@
 import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../Redux/store';
 import { reviewsInit } from '../../Redux/Reducers/RewiewSlice';
+import './ReviewList.css';
 
 function ReviewList(): JSX.Element {
-  const [num, setNum] = useState(1);
+  const [num, setNum] = useState(0);
   const { rewiews } = useAppSelector((store) => store.rewiews);
   const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    dispatch(reviewsInit());
+    const fetchReviews = async () => {
+      await dispatch(reviewsInit());
+      setLoading(false);
+    };
+
+    fetchReviews();
   }, []);
-  
-  console.log(rewiews);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="ReviewListmain">
-      <div>Отзывы</div>
+      <button
+        className="butmin"
+        type="button"
+        onClick={() => {
+          if (num > 0) {
+            setNum(num - 1);
+          }
+        }}
+      >
+        ᐸ
+      </button>
+      <div className="textall">
+        <div className="nameofrew">{rewiews[num].userName}</div>
+        <div className="textofrew">{rewiews[num].reviewText}</div>
+      </div>
+
+      <button
+        type="button"
+        className="butplus"
+        onClick={() => {
+          if (num <= rewiews.length - 2) {
+            setNum(num + 1);
+          }
+        }}
+      >
+        ᐳ
+      </button>
     </div>
   );
 }
