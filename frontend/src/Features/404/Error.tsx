9b0618c4a58d0ebbf051689d2e7eaf58dace31bc
pu99r <iiigor.mik@gmail.com>
2023-07-07 14:32:33 +0300

    1

--- frontend/src/Features/404/Error.tsx
+++ frontend/src/Features/404/Error.tsx
@@ -1,16 +1,25 @@
 import React, { useEffect } from 'react';
 import logo from './logo/baltika kids logo-round.png';
 import './Error.css';
+import { useNavigate } from 'react-router-dom';
 function Error(): JSX.Element {
+  const navigate = useNavigate();
+  const goBack = () => navigate(-1);
+
   return (
     <div>
       <div className="er4text">
         <h1 className="er4">4</h1>
-        <img className='imgerror' src={logo} alt="f" />
+        <img className="imgerror" src={logo} alt="f" />
         <h1 className="er4">4</h1>
       </div>
       <div>
         <h1 className="opps">Упс! Страница не найдена...</h1>
+      </div>
+      <div className="butdiv">
+        <button className="buttonopps" type="submit" onClick={goBack}>
+          Назад
+        </button>
       </div>
     </div>
   );
